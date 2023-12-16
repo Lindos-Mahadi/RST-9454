c4d3b4dd8456b17dab2acd4f5ddaac4183f32ad0
lindos mahadi <lindosmahadi@gmail.com>
2023-12-16 11:35:37 +0600

    Create AppDomain layer

---
+++ AppDomainLayer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppDomainLayer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
