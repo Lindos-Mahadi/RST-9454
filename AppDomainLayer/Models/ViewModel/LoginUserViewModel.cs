ec2123c9053b02abc2c9f69b7598572fce6ad118
Lindos-Mahadi <lindosmahadi@gmail.com>
2023-12-20 14:12:28 +0600

    Change Password property in User Model verify and Rename LoginView Model

---
+++ AppDomainLayer/Models/ViewModel/LoginUserViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppDomainLayer.Models.ViewModel
+{
+    public class LoginUserViewModel
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+        [Required]
+        [Display(Name = "User Password")]
+        public string Password { get; set; }
+        [Display(Name = "Remember Me?")]
+        public bool IsRemember { get; set; }
+    }
+}
